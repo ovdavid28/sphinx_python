c37136d5b78d71c53bc1705b0a5184338b04a6f6
Ordoñez Vivar <david.ordonez.vivar@accenture.com>
2019-06-02 09:45:34 +0200

    adding functions

---
+++ Example.py
@@ -0,0 +1,21 @@
+
+
+def func1(arg1, arg2):
+    """
+    This function takes to arguments, set the first equal to second and then return the first argument.
+    :param arg1: Some value
+    :param arg2: Another value
+
+    :return: arg1
+    """
+    arg1 = arg2
+    return arg1
+
+
+def func2():
+    """
+    Function with no arguments
+
+    :return: returns None
+    """
+    return None
